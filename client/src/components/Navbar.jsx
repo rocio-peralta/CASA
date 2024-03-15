9f396162baa3ec1bd29bff5aa9b94d236cd8e3cb
Rocio-Peralta <1rocioperalta@gmail.com>
2024-03-16 08:43:12 +1300

    added favicon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,27 +12,7 @@ const Navbar = () => {
             SANCTUARY
           </Link>
 
-          <svg
-            viewBox="0 0 6 6"
-            id="meteor-icon-kit__solid-circle-xxxs"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M3 6C1.3431 6 0 4.6569 0 3C0 1.3431 1.3431 0 3 0C4.6569 0 6 1.3431 6 3C6 4.6569 4.6569 6 3 6z"
-                fill="#C8C2AF"
-              ></path>
-            </g>
-          </svg>
+          <svg width="7px" height="7px" viewBox="0 0 6 6" id="meteor-icon-kit__solid-circle-xxxs" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path fillRule="evenodd" clipRule="evenodd" d="M3 6C1.3431 6 0 4.6569 0 3C0 1.3431 1.3431 0 3 0C4.6569 0 6 1.3431 6 3C6 4.6569 4.6569 6 3 6z" fill="#C8C2AF"></path></g></svg>
         </div>
         {/* <div>
        <Link to = '/about'>About</Link><br></br>
