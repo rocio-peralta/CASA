03c40220ca137d4f03b1a977ceaebaec4af651be
Rocio-Peralta <1rocioperalta@gmail.com>
2024-03-23 09:07:59 +1300

    finished responsive design in home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,22 +4,23 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <section className="grid grid-cols-4 font-typographicSecondary text-3xl sm:text-6xl font-medium text-secondaryColor">
-        <div className="col-span-3 pt-10 overflow-hidden">
-          <h1 className="p-5 animate-reveal">DESIGNED</h1>
+
+      <section className="grid grid-cols-1 sm:grid-cols-4 font-typographicSecondary text-4xl sm:text-6xl font-medium text-secondaryColor">
+        <div className=" col-span-1 sm:col-span-3 sm:pt-10 overflow-hidden">
+          <h1 className="pl-5 sm:p-5 animate-reveal">DESIGNED</h1>
         </div>
-        <div className="pt-10 overflow-hidden">
-          <h1 className="grid justify-items-end p-5 pl-5 pr-5 animate-reveal">
+        <div className=" sm:pt-10 col-span-1 sm:col-start-4 sm:col-end-5 overflow-hidden">
+          <h1 className="grid sm:justify-items-end pl-5 pr-5 sm:p-5 sm:pl-5   sm:pr-5 animate-reveal">
             AND
           </h1>
         </div>
-        <div className="col-start-1 col-end-5 overflow-hidden">
+        <div className="sm:col-start-1 sm:col-end-5 overflow-hidden">
           <h1 className="grid justify-items-end pl-5 pr-5 animate-reveal">
             CRAFTED IN THE
           </h1>
         </div>
-        <div className="col-start-3 col-end-5 pt-4 overflow-hidden">
-          <h1 className="grid justify-items-end  text-secondaryColor font-typographicTertiary text-xl sm:text-3xl font-thin line-through pr-5 animate-reveal">
+        <div className="sm:col-start-3 sm:col-end-5 pt-4 overflow-hidden">
+          <h1 className="grid justify-items-end font-typographicTertiary text-xl sm:text-3xl font-thin line-through pr-5 animate-reveal">
             BAY OF PLENTY
           </h1>
         </div>
