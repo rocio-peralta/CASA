5c1412219a5425b162fd602fedd51df82aabf1e0
Rocio-Peralta <1rocioperalta@gmail.com>
2024-03-24 11:29:26 +1300

    working on frame emotion

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,23 @@
 import Navbar from './Navbar'
 import Transition from './Transition'
-import {motion} from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react'
 
 const Home = () => {
+  const targetRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ['start end', 'end end'],
+  })
+  //This is a variable or value representing the progress of scrolling on the Y-axis.
+ // the first array is the range of the scrollYProgress, the second array is the range of the opacity. 
+ // firs array indi
+  const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [0, 0.5, 1])
   return (
     <>
       <Navbar />
 
-      <motion.section className="grid grid-cols-1 sm:grid-cols-4 font-typographicSecondary text-4xl sm:text-6xl font-medium text-secondaryColor">
+      <section className="relative grid grid-cols-1 sm:grid-cols-4 font-typographicSecondary text-4xl sm:text-6xl font-medium text-secondaryColor">
         <div className=" col-span-1 sm:col-span-3 sm:pt-10 overflow-hidden">
           <h1 className="pl-5 sm:p-5 animate-reveal">DESIGNED</h1>
         </div>
@@ -26,9 +36,31 @@ const Home = () => {
             BAY OF PLENTY
           </h1>
         </div>
-        <Transition />
-      </section>
 
+        <motion.img
+          style={{ opacity }}
+          ref={targetRef}
+          className="justify-end pt-20"
+          src="/images/wood.jpeg"
+          alt="Wood"
+        ></motion.img>
+        <br />   <br />    <br />
+          <motion.img
+          style={{ opacity }}
+          ref={targetRef}
+          className="justify-end pt-20"
+          src="/images/wood.jpeg"
+          alt="Wood"
+        ></motion.img>
+          <br />   <br />    <br />
+          <motion.img
+          style={{ opacity }}
+          ref={targetRef}
+          className="justify-end pt-20"
+          src="/images/wood.jpeg"
+          alt="Wood"
+        ></motion.img>
+      </section>
     </>
   )
 }
--- client/src/components/Transition.jsx
+++ client/src/components/Transition.jsx
@@ -2,8 +2,10 @@
 
 const Transition = () => {
   return (
-    <div className="col-span-1 sm:col-start-4 sm:col-end-5 ">
-      <img className="justify-end pt-20" src="/images/wood.jpeg" alt="Wood" />
+    <div
+      className="col-span-1 sm:col-start-4 sm:col-end-5 "
+    >
+      <img className="justify-end pt-20" src="/images/wood.jpeg" alt="Wood" ></img>
     </div>
   )
 }
