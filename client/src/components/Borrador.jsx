fa0c1aed11b0860345b0fc0a7e26b45b48838332
Rocio-Peralta <1rocioperalta@gmail.com>
2024-03-26 20:57:53 +1300

    trying styling

---
+++ client/src/components/Borrador.jsx
@@ -0,0 +1,91 @@
+const Borrador = () => {
+  return (
+    <>
+      <main>
+        <div className="works__container enterContents">
+          <section className="works__containerSec">
+            <div className="works__containerSecBlock">
+              <div>
+                <article
+                  className="works__containerSecBlockBox"
+                  style="top: 0%;"
+                >
+                  <div>
+                    <a
+                      className="works__containerSecBlockBoxInner magnetic"
+                      href="/works/miroku"
+                    >
+                      <figure className="works__containerSecBlockBoxInnerFig">
+                        <img
+                          src="https://images.microcms-assets.io/assets/787b219dbb384d9da4a70d8ed4e65908/471fadbed082451c8868a789b9c626c6/miroku_main.jpg"
+                          alt="miroku"
+                          style="transform: translate(0px, 0px);"
+                        ></img>
+                      </figure>
+                      <div className="works__containerSecBlockBoxInnerTxt">
+                        <div
+                          className="works__containerSecBlockBoxInnerTxtIn"
+                          style="transform: translate(0px, 0px);"
+                        >
+                          <h2>miroku</h2>
+                          <span>2023</span>
+                        </div>
+                      </div>
+                      <span
+                        className="cmn__mouse"
+                        style="transform: translate(0px, 0px);"
+                      >
+                        <span>
+                          <span>View</span>
+                        </span>
+                      </span>
+                    </a>
+                  </div>
+                </article>
+              </div>
+
+              <div>
+                <article
+                  className="works__containerSecBlockBox"
+                  style="top: -30%;"
+                >
+                  <div>
+                    <a
+                      className="works__containerSecBlockBoxInner magnetic"
+                      href="/works/biosummit"
+                    >
+                      <figure className="works__containerSecBlockBoxInnerFig">
+                        <img
+                          src="https://images.microcms-assets.io/assets/787b219dbb384d9da4a70d8ed4e65908/889c7510481249ea86b3136f2ceebfcb/main.png"
+                          alt="BIO SUMMIT"
+                          style="transform: translate(0px, 0px);"
+                        ></img>
+                      </figure>
+                      <div className="works__containerSecBlockBoxInnerTxt">
+                        <div
+                          className="works__containerSecBlockBoxInnerTxtIn"
+                          style="transform: translate(0px, 0px);"
+                        >
+                          <h2>BIO SUMMIT</h2>
+                          <span>2023</span>
+                        </div>
+                      </div>
+                      <span
+                        className="cmn__mouse"
+                        style="transform: translate(0px, 0px);"
+                      >
+                        <span>
+                          <span>View</span>
+                        </span>
+                      </span>
+                    </a>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </>
+  )
+}
