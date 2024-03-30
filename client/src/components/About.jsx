c8daeb8609c985a20dac6c920772a01feecb1f4a
Rocio-Peralta <1rocioperalta@gmail.com>
2024-03-30 19:14:32 +1300

    add negative values to img

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -38,9 +38,9 @@ const About = () => {
             </h1>
           </div>
         </motion.section>
-        <div className="mt-5 grid grid-cols-1 sm:grid-cols-8 font-typographicSecondary text-lg sm:text-xl text-secondaryColor items-end">
-          <div className=" col-span-1 sm:col-span-3 sm:col-start-6 sm:pt-10">
-            <h1 className="pl-5 sm:p-5 text-justify">
+        <div className="mt-5 grid grid-cols-1 sm:grid-cols-8 font-typographicSecondary text-lg sm:text-2xl text-secondaryColor items-end">
+          <div className="col-span-1 sm:col-span-3 sm:col-start-6 sm:pt-10">
+            <h1 className="pl-5 sm:p-5 ">
               Our Online Design Tool it’s a simple as dragging our pre-design
               modules into the center of your screen and configuring them how
               you like.
@@ -49,7 +49,7 @@ const About = () => {
 
           <div className="grid justify-items-start sm:col-start-6 pl-5 sm:col-end-9 pt-4 overflow-hidden hover:animate-bounce ">
             <Link to="/about">
-              <h1 className=" font-typographicTertiary text-xl sm:text-3xl font-thin ">
+              <h1 className="font-typographicTertiary text-xl sm:text-3xl font-thin ">
                 About us
               </h1>
               <div className="pt-3 sm:col-start-6">
@@ -58,10 +58,10 @@ const About = () => {
             </Link>
           </div>
 
-          <div className="sm:col-start-1 sm:col-end-4 bg-slate-400 ">
+          <div className="sm:col-start-1 sm:col-end-4 -m-10 bg-slate-400 ">
             <img className="" src="/images/concrete.jpeg" alt="wood" />
           </div>
-          <div className="grid items-end sm:col-start-7 sm:col-end-9 h-64 mr-10 bg-slate-600">
+          <div className="grid items-end sm:col-start-7 sm:col-end-9 h-64 -mr-10 bg-slate-600">
             hola
           </div>
         </div>
